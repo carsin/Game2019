baa561b1643ffd31c5a480a74392adbc91cbca14
bananatrooper52 <bananatrooper52@gmail.com>
2019-02-18 22:58:16 -1000

    Chunked tile transmission and rendering

--- src/server.js
+++ src/server.js
@@ -4,7 +4,8 @@ var http = require("http").Server(app);
 var io = require("socket.io")(http);
 
 // World variables
-var world = require(__dirname + "/world.js");
+var World = require(__dirname + "/world.js");
+var world = new World(200, 10);
 
 // Send html
 app.use(express.static(__dirname + "/client"));
@@ -18,8 +19,17 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
     console.log("A player connected");
 
+    io.emit("worldData", { mapSize: world.worldSize, chunkSize: world.chunkSize });
+
     // Send map to player initally
-    io.emit("load map", world.world)
+    var chunksToSend = [];
+    for (var x = 0; x < world.lengthInChunks; x++) {
+        for (var y = 0; y < world.lengthInChunks; y++) {
+            chunksToSend.push(world.getChunk(x, y));
+        }
+    }
+
+    io.emit("chunks", chunksToSend);
 
     // Disconnect event
     socket.on("disconnect", () => {
--- src/world.js
+++ src/world.js
@@ -1,19 +1,61 @@
-var world = {
-    map: [],
-    xMax: 200,
-    yMax: 200,
+function Chunk(chunkSize, chunkX, chunkY) {
+    this.updated = true; // Whether this chunk has been changed since last transmission
+    this.tiles = [];
+    this.chunkX = chunkX;
+    this.chunkY = chunkY;
+    this.chunkSize = chunkSize;
 
-    initMap: () => {
-        console.log("Building map with size " + world.xMax + "x" + world.yMax);
-        for (var x = 0; x <= world.xMax; x++) {
-            world.map[x] = [];
-            for (var y = 0; y <= world.yMax; y++) {
-                world.map[x][y] = String(Math.round(Math.random()));
-            }
+    // Set and get tiles
+    this.setTile = (x, y, val) => {
+        tiles[x][y] = val;
+        this.updated = true;
+    };
+
+    this.getTile = (x, y) => {
+        return tiles[x][y];
+    };
+
+    // Init tiles
+    for (var x = 0; x < this.chunkSize; x++) {
+        this.tiles[x] = [];
+        for (var y = 0; y < this.chunkSize; y++) {
+            this.tiles[x][y] = Math.floor(Math.random() * 2);
         }
     }
 }
 
-world.initMap();
+function World(worldSize, chunkSize) {
 
-module.exports = { world }
+    // Chunks array
+    this.chunks = [];
+
+    // Length of the world in tiles
+    this.worldSize = worldSize;
+
+    // Tiles per chunk
+    this.chunkSize = chunkSize;
+    
+    // Length of the world in chunks
+    this.lengthInChunks = this.worldSize / this.chunkSize;
+
+    this.getChunk = (x, y) => { return this.chunks[x][y]; };
+
+    // Automatically set and get tile from raw tile coords
+    this.setTile = (x, y, val) => {
+        getChunk(Math.floor(x / this.chunkSize), Math.floor(y / this.chunkSize)).setTile(x % this.chunkSize, y % this.chunkSize, val);
+    };
+
+    this.getTile = (x, y) => {
+        return getChunk(Math.floor(x / this.chunkSize), Math.floor(y / this.chunkSize)).getTile(x % this.chunkSize, y % this.chunkSize);
+    };
+
+    // Init chunks
+    for (var x = 0; x < this.worldSize / this.chunkSize; x++) {
+        this.chunks[x] = [];
+        for (var y = 0; y < this.worldSize / this.chunkSize; y++) {
+            this.chunks[x][y] = new Chunk(this.chunkSize, x, y);
+        }
+    }
+}
+
+module.exports = World;
