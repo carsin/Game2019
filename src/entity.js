eba7c4e8522c0a8338b145b560cfd7051e0c68f8
carsin <carsonfreedman1@gmail.com>
2019-02-19 21:07:27 -1000

    start entity movement

--- src/entity.js
+++ src/entity.js
@@ -3,6 +3,26 @@ function Entity(name, id, initialX, initialY, world) {
     this.id = id;
     this.x = initialX;
     this.y = initialY;
+    this.lastDirSwitched = 0;
+    this.dir = 4;
+
+    this.update = () => {
+        // Random movement
+        this.lastDirSwitched++;
+        
+        if (this.lastDirSwitched >= 120) {
+            this.dir = Math.floor(Math.random() * 5);
+        }
+
+        switch (dir) {
+            case 0: this.x++;
+            case 1: this.x--;
+            case 2: this.y++;
+            case 3: this.y--;
+            case 4: console.log("staystillbah");
+        }
+
+    };
 
 
     world.entities.push(this);
